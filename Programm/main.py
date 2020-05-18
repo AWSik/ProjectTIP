dfd4d8ad0e26bef13d9c5a61559b7801c19910b6
Andrey <aws1998@ya.ru>
2020-05-18 12:24:23 +0500

    Create main - programm

---
+++ Programm/main.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtWidgets
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleApp()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
