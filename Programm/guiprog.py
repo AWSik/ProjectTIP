18061596b940824c29c038da68ce7438dab7c9f5
Andrey <aws1998@ya.ru>
2020-05-18 12:24:58 +0500

    Create file with paint GUI - programm

---
+++ Programm/guiprog.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+from PyQt5 import QtWidgets
+
+import design
+
+class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.btnBrowse.clicked.connect(self.browse_folder)
+
+    def browse_folder(self):
+        self.listWidget.clear()
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
+
+        if directory:
+            for file_name in os.listdir(directory):
+                self.listWidget.addItem(file_name)
